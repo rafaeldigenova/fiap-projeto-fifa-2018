e01901b2860b9b526d2e6df3b9dc91123336c554
Lucas Gomes <lucasluismgomes@gmail.com>
2018-05-01 00:45:21 -0300

    Task -> autenticação e autorização (parcial)

---
+++ Fiap.ProjetoFifa2018.Dominio/Entidades/Account/TokenInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace Fiap.ProjetoFifa2018.Dominio.Entidades.Account
+{
+    public class TokenInfo
+    {
+        public string password;
+        public string username;
+    }
+}
--- Fiap.ProjetoFifa2018/Controllers/AccountController.cs
+++ Fiap.ProjetoFifa2018/Controllers/AccountController.cs
@@ -1,21 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Fiap.ProjetoFifa2018.Web.Controllers
 {
     public class AccountController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
+            if (User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Times");
+            }
+
             return View();
         }
 
-        public IActionResult Denied()
+        [HttpPost]
+        public async Task<IActionResult> Index(ViewModel.LoginViewModel model)
         {
-            return View();
+            if (model.UserName == "rodolfo" && model.Password == "123")
+            {
+                var claims = new List<Claim>();
+                claims.Add(new Claim(ClaimTypes.Name, model.UserName));
+                var id = new ClaimsIdentity(claims, "password");
+                claims.Add(new Claim(ClaimTypes.Role, "admin"));
+                var principal = new ClaimsPrincipal(id);
+
+                await HttpContext.SignInAsync("app", principal, new AuthenticationProperties() { IsPersistent = model.IsPersistent });
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View("NotFound");
+        }
+
+        public async Task<IActionResult> Logoff()
+        {
+            await HttpContext.SignOutAsync();
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
